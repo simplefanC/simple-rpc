2214b33bde016df98e8ab4c9436899e269d393ea
chenfan <simplefanc@gmail.com>
2022-10-24 22:34:23 +0800

    implement Kryo Serializer

--- framework/core/src/main/java/com/simplefanc/serialize/kyro/KryoSerializer.java
+++ framework/core/src/main/java/com/simplefanc/serialize/kyro/KryoSerializer.java
@@ -1,19 +1,58 @@
 package com.simplefanc.serialize.kyro;
 
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import com.simplefanc.exception.SerializeException;
+import com.simplefanc.remoting.dto.RpcRequest;
+import com.simplefanc.remoting.dto.RpcResponse;
 import com.simplefanc.serialize.Serializer;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
 /**
+ * Kryo 序列化
+ *
  * @author chenfan
  * @date 2022/10/18 16:40
  **/
 public class KryoSerializer implements Serializer {
+    /**
+     * 保证 Kryo 的线程安全
+     */
+    private final ThreadLocal<Kryo> kryoThreadLocal = ThreadLocal.withInitial(() -> {
+        Kryo kryo = new Kryo();
+        kryo.register(RpcResponse.class);
+        kryo.register(RpcRequest.class);
+        return kryo;
+    });
+
     @Override
     public byte[] serialize(Object obj) {
-        return new byte[0];
+        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+             Output output = new Output(byteArrayOutputStream)) {
+            Kryo kryo = kryoThreadLocal.get();
+            // Object->byte:将对象序列化为byte数组
+            kryo.writeObject(output, obj);
+            kryoThreadLocal.remove();
+            return output.toBytes();
+        } catch (Exception e) {
+            throw new SerializeException("Serialization failed");
+        }
     }
 
     @Override
     public <T> T deserialize(byte[] bytes, Class<T> clazz) {
-        return null;
+        try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+             Input input = new Input(byteArrayInputStream)) {
+            Kryo kryo = kryoThreadLocal.get();
+            // byte->Object:从byte数组中反序列化出对对象
+            Object o = kryo.readObject(input, clazz);
+            kryoThreadLocal.remove();
+            return clazz.cast(o);
+        } catch (Exception e) {
+            throw new SerializeException("Deserialization failed");
+        }
     }
 }
